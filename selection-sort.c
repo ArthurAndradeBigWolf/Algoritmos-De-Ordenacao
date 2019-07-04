0288cc46ff7e33325ade40d53dd6452fad939bec
Arthur Vinicius Fogaça de Andrade <arthurandradenf@gmail.com>
2019-07-04 11:40:03 -0300

    Comentando Selection-sort.c

--- selection-sort.c
+++ selection-sort.c
@@ -2,17 +2,21 @@
 #include<stdio.h>
 
 
+//ASSINATURA DO ALGORITMO DE ORDENAÇÃO SELECTION
 void selection(int *vetor, int n, FILE *arquivo);
 
 
 
 int main(int argc, char const *argv[])
-{
-    int vetor[] = {10,7,2,1,4};
-    int n = 5;
+{   
+    
+    int vetor[] = {10,7,2,1,4}; //VETOR DESORDENADO 
+    int n = 5;//TAMNHO DO VETOR
     FILE* arquivo;
 
+    //ARQUIVO ONDE CONTERÁ AS INFORMAÇÕS DO SELECTION
     arquivo = fopen("dadosSelection.csv","w");
+
 
     fputs("Vetor >> ", arquivo);
     for(int i=0; i<n; i++){
@@ -33,7 +37,10 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+
+//ALGORITMOS SELECTION
 void selection(int *vetor, int n, FILE *arquivo){
+    
     int menor;
     int aux;
 
@@ -46,11 +53,13 @@ void selection(int *vetor, int n, FILE *arquivo){
             }
         }
 
+        //ESCREVENDO NO ARQUIVO AS TROCAS
         fprintf(arquivo,"Trocou %d que está na posica %d, com %d da posicao %d \n",vetor[i],i,vetor[menor],menor);
         aux = vetor[i];
         vetor[i] = vetor[menor];
         vetor[menor] = aux;
 
+        //ESCREVENDO O VETOR APÓS A TROCA
         for(int k = 0; k<n; k++){
             fprintf(arquivo,"%d, ", vetor[k]);
         }
